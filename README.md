733a997e98d9b7ed2bd3b80aa57171d76880f7fd
Farouk Bakari <farouk.baccari.externe@emeria.eu>
2025-10-03 23:10:21 +0200

    feat: best libs on web

--- README.md
+++ README.md
@@ -40,3 +40,8 @@ npm run dev
 ```sh
 npm run build
 ```
+
+### Interresting links to study
+https://github.com/frankykevin/pump-fun-frontend/tree/main/public/libraries
+https://github.com/hadialaddin/crypto-genie/tree/713ccb891038b380e2fb15e4b178c516c79260ef/dashboard/charting_library_clonned_data/charting_library
+https://github.com/rubenmarcus/malicious-repositories/tree/a853975af9dceaae817911ad27123d349142ced0/DEX-staking-project-ultrax/public/charting_library *** (older 2022 but multi-language)
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,29 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    // middlewareMode: true, // Removed to avoid "cannot print server URLs in middleware mode" error
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+    {
+      name: 'custom-404-logger',
+      configureServer(devServer) {
+        devServer.middlewares.use((req, res, next) => {
+          res.on('finish', () => {
+            if (res.statusCode === 404) {
+              console.warn(`404: ${req.originalUrl}`);
+            }
+          });
+          next();
+        });
+      }
+    }
+  ]
 })
