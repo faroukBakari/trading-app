c04304a75788c5e99136cd637afda28fbc9875e0
Farouk Bakari <farouk.baccari.externe@emeria.eu>
2025-10-03 20:55:19 +0200

    feat: working charting library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { version } from '../../public/charting_library'
+// Make sure the path is correct; update if necessary
+import TVChartContainer from '../components/TVChartContainer.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Welcome to Trading App!</h1>
+    <p>Charting library version: {{ version() }}</p>
+    <TVChartContainer />
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+  padding: 2rem;
+}
+
+h1 {
+  color: #42b983;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  font-weight: 600;
+}
+
+p {
+  font-size: 1.5rem;
+  color: #2c3e50;
+}
+</style>
